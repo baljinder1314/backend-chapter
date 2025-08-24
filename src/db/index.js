5d40457ed5876a3f76d598efbb3b212e672bd40a
Baljinder-Singh <baljindersingh987209@gmail.com>
2025-08-24 12:58:57 +0530

    feat: set up database connection and environment configuration
    - Added dotenv and mongoose as dependencies in package.json.
    - Created a constants.js file to store application constants.
    - Implemented a connectDB function in a new db/index.js file to handle MongoDB connection.
    - Updated index.js to configure dotenv and establish a database connection.
    - Commented out initial express setup for future implementation.

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { APP_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionString = await mongoose.connect(
+      `${process.env.MONGO_DB}/${APP_NAME}`
+    );
+    console.log(`Mongoose connected Successfuly ${connectionString.Connection.host}`)
+  } catch (error) {
+    console.log(`MongoDB didn't connet: ${error}`);
+  }
+};
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("hello ji")
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({path:"./.env"});
+
+connectDB();
+
+/*
+import express from "express";
+
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_DB}/${APP_NAME}`);
+    app.on("error", (error) => {
+      console.log(error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.log("ERROR: ", error);
+    throw error;
+  }
+})();
+*/
